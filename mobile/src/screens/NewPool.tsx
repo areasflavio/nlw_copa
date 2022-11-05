5c5d6ce7880e1fcd77aba28c35bc85904e01e650
Flávio Arêas <vflavio9@gmail.com>
2022-11-04 21:10:03 -0300

    feat: connect pools screens to api

--- mobile/src/screens/NewPool.tsx
+++ mobile/src/screens/NewPool.tsx
@@ -1,19 +1,52 @@
 import { useState } from 'react';
-import { Heading, Text, VStack } from 'native-base';
+import { Heading, Text, useToast, VStack } from 'native-base';
 
 import Logo from '../assets/logo.svg';
 import { Header } from '../components/Header';
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
+import { api } from '../services/api';
 
 export function NewPool() {
   const [poolTitle, setPoolTitle] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleCreatePool = () => {
-    console.log({
-      poolTitle,
-    });
+  const toast = useToast();
+
+  const handleCreatePool = async () => {
+    if (!poolTitle.trim()) {
+      return toast.show({
+        title: 'Informe um nome para o seu bolão.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+
+    try {
+      setIsLoading(true);
+
+      await api.post('/pools', { title: poolTitle });
+
+      toast.show({
+        title: 'Bolão criado com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500',
+      });
+
+      setPoolTitle('');
+    } catch (err) {
+      console.error(err);
+
+      toast.show({
+        title: 'Não foi possível criar seu bolão. Tente novamente.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsLoading(false);
+    }
   };
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Criar novo bolão" />
@@ -38,7 +71,11 @@ export function NewPool() {
           placeholder="Qual o nome do seu bolão?"
         />
 
-        <Button title="Criar meu bolão" onPress={handleCreatePool} />
+        <Button
+          title="Criar meu bolão"
+          onPress={handleCreatePool}
+          isLoading={isLoading}
+        />
 
         <Text color="gray.200" fontSize="sm" textAlign="center" px={10} mt={4}>
           Após criar seu bolão, você receberá um código único que você poderá
--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,12 +1,46 @@
-import { Icon, VStack } from 'native-base';
+import { useCallback, useEffect, useState } from 'react';
+import { FlatList, Icon, useToast, VStack } from 'native-base';
 import { Octicons } from '@expo/vector-icons';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
-import { useNavigation } from '@react-navigation/native';
+import { api } from '../services/api';
+import { Loading } from '../components/Loading';
+import { PoolCard, PoolCardPros } from '../components/PoolCard';
+import { EmptyPoolList } from '../components/EmptyPoolList';
 
 export function Pools() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardPros[]>([]);
+
   const navigation = useNavigation();
+  const toast = useToast();
+
+  const fetchPools = async () => {
+    try {
+      setIsLoading(true);
+      const response = await api.get('/pools');
+
+      setPools(response.data.pools);
+    } catch (err) {
+      console.error(err);
+
+      toast.show({
+        title: 'Não foi possível carregar os bolões. Tente novamente.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchPools();
+    }, [])
+  );
 
   return (
     <VStack flex={1} bgColor="gray.800">
@@ -28,6 +62,20 @@ export function Pools() {
           onPress={() => navigation.navigate('find')}
         />
       </VStack>
+
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          data={pools}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <PoolCard data={item} />}
+          px={5}
+          _contentContainerStyle={{ pb: 10 }}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={() => <EmptyPoolList />}
+        />
+      )}
     </VStack>
   );
 }
